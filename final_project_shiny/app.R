278b4357cc73ce686965cbdd92e095d2d0735ea0
phannahhan <hannahphan@college.harvard.edu>
2020-04-26 23:43:37 -0400

    rough draft completed

--- final_project_shiny/app.R
+++ final_project_shiny/app.R
@@ -415,8 +415,11 @@ ui <- fluidPage(
     
     tabPanel(
       title = "About",
-      h3("How Did I Conduct This Analysis?")
-    )
+      h3("How Did I Conduct This Analysis?"),
+      p(
+        "My data came from a variety of publicly viewable datasets created by Analyze Boston and from the City of Boston’s website. For the geospatial l data, I needed to standardize the names of the columns for Longitude and Latitude to combine my datasets as well as remove N/A values and header rows that were not actually data points. For my statistical analysis, I calculated the crime densities of Suffolk County by dividing the region into 21 zones. I compared this to the absolute number of streetlights, trees, and Bluebikes in each zone and applied a linear regression model between the crime count and each of the public amenities. At first, I wanted to divide the map into a square grid and calculate the densities of each square, but then I realized that squares that included land outside of Suffolk County would inaccurately have a lower density of each amenity in addition to crime levels, so I made the decision to pick square zones of equal area that only included Suffolk County. When creating my regression table, I chose to take the logs of each value because there were significantly fewer bikes to crimes as there were trees or streetlights to crimes. Taking the log of each value allowed me to see the percent change in crime density as the explanatory variable increased by one percentage point."
+      )
+    ),
     
     tabPanel(
       title = "Public Infrastructure and Amenity Relations",
